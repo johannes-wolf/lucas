5491bb3a7038ccc97258839511dde0e880c3b268
Johannes Wolf <mail@johannes-wolf.com>
2022-10-30 09:04:05 +0100

    poly: Test poly division

--- test/test-poly.lua
+++ test/test-poly.lua
@@ -12,4 +12,9 @@ function tests.poly_degree()
   Expect('poly.deg(3x^4+x^2,x)', '4')
 end
 
+function tests.poly_div()
+  Expect('poly.div(5x^2+4x+1,2x+3,x)',       '{-7:4+5:2x,25:4}')
+  Expect('poly.div(x^3-12x^2+5x+150,x-5,x)', '{x^2-7x-30,0}')
+end
+
 return tests
